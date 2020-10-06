7a5fbce4db6db6f10899e07dd752863d44e94061
FakhriAm <fakhri_muhammad93@yahoo.com>
2020-10-06 13:57:15 +0700

    renew - content detail

--- application/views/main/video_detail.php
+++ application/views/main/video_detail.php
@@ -3,11 +3,11 @@
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <div class="container-fluid">
-      <div class="row mb-2">
+      <!-- <div class="row mb-2">
         <div class="col-sm-6">
           <h1>Video Detail</h1>
         </div>
-      </div>
+      </div> -->
     </div><!-- /.container-fluid -->
   </section>
 
@@ -17,7 +17,7 @@
       <div class="card">
         <div class="card-header">
           <div class="card-title">
-            Video Information
+            <h5 class="m-0 font-weight-bold">Video Information</h5>
           </div>
         </div>
         <div class="card-body">
@@ -27,23 +27,12 @@
               </source>
             </video>
           </div>
-          </br>
-          <div class="row">
-            <div class="col-2">
-              <h3><?php echo $data->video_title ?></h3>
-            </div>
+          <br>
+       
 
-          </div>
-          <div class="row">
-            <div class="col-2">
-              <span>CNN Indonesia</span>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-2">
-              <span>Almer P.S</span>
-              <hr />
-            </div>
+
+        <!-- <div class="row">
+
             <div class="col-10">
               <a href="<?php echo base_url('video/downloadVideo?link=' . $link) ?>">
                 <button class="btn btn-primary float-right btn-icon-split">
@@ -55,30 +44,99 @@
                   </span>
                 </button>
               </a></div>
+          </div> -->
+        <!-- <h4>Video Description</h4>
+          <p></p>
+          <hr /> -->
+        <div class="card">
+          <!-- Card Header - Accordion -->
+          <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+            <h6 class="m-0 font-weight-bold">Video Description</h6>
+          </a>
+          <!-- Card Content - Collapse -->
+          <div class="collapse show" id="collapseCardExample">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-6">
+                  <h3><?php echo  $data->video_title ?></h3>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <span>CNN Indonesia</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <span><?php echo $data->journalist ?></span>
+                  <hr />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <?php echo $data->description ?>
+                  <hr />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-2">
+                  <span class="icon">
+                    <i class="fas fa-tags"></i>
+                    Tags
+                  </span>
+                  <span>
+                    <?php echo $badge ?>
+                  </span>
+                </div>
+                <!-- <div class="col-2">
+                  <div class="col-10"><?php echo $badge ?></div>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          <div class="card-footer">
+
+            <!-- <a href="#">
+              <button class="btn btn-warning float-right btn-icon-split">
+                <span class="icon">
+                  <i class="far fa-credit-card"></i>
+                </span>
+                <span class="text">
+                  Buy
+                </span>
+              </button>
+            </a> -->
+
+            <a href="<?php echo base_url('video/downloadVideo?link=' . $link) ?>">
+              <button class="btn btn-success float-right btn-icon-split">
+                <span class="icon text-white">
+                  <i class="fas fa-download"></i>
+                </span>
+                <span class="text">
+                  Download
+                </span>
+              </button>
+            </a>
           </div>
 
-          <p><?php echo $data->description ?></p>
-          <hr />
-
-
         </div>
-      </div>
+        </div>
   </section>
   <!-- /.content -->
-</div>
-<!-- /.content-wrapper-->
-<script type="text/javascript">
-  var options = {};
 
-  var player = videojs('my-player', options, function onPlayerReady() {
-    videojs.log('Your player is ready!');
+  <!-- /.content-wrapper-->
+  <script type="text/javascript">
+    var options = {};
 
-    // In this context, `this` is the player that was created by Video.js.
-    this.play();
+    var player = videojs('my-player', options, function onPlayerReady() {
+      videojs.log('Your player is ready!');
 
-    // How about an event listener?
-    this.on('ended', function() {
-      videojs.log('Awww...over so soon?!');
+      // In this context, `this` is the player that was created by Video.js.
+      this.play();
+
+      // How about an event listener?
+      this.on('ended', function() {
+        videojs.log('Awww...over so soon?!');
+      });
     });
-  });
-</script>
+  </script>
